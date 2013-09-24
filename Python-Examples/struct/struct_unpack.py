f82245fa9ea79b6b616735307610e4d7fbde40b9
Serdar Dalgıç <sd@serdardalgic.org>
2013-09-24 10:46:30 +0300

    Snippets about struct usage in python

---
+++ Python-Examples/struct/struct_unpack.py
@@ -0,0 +1,8 @@
+import struct
+import binascii
+
+packed_data = binascii.unhexlify('0100000061620000cdcc2c40')
+
+s = struct.Struct('I 2s f')
+unpacked_data = s.unpack(packed_data)
+print 'Unpacked Values:', unpacked_data
