879e61a7b79a949ed2ca82b05b516a64ee9e739b
Serdar Dalgıç <sd@serdardalgic.org>
2013-10-31 00:59:42 +0200

    Calculate length of strings test.

--- Python-Examples/bigfilesearch/tests.py
+++ Python-Examples/bigfilesearch/tests.py
@@ -1,9 +1,14 @@
 import unittest
+import filesearch
 
 
 class BigFileSearchTest(unittest.TestCase):
-    def test_test(self):
-        self.assertEqual(2, 1 + 1)
+    def test_calculate_len_of_strs(self):
+        string = "Red$Beacon$Rocks"
+        exp_rv = "3 6 5"
+        rv = filesearch.calculate_len_of_strs(
+            string.strip().split('$'))
+        self.assertEqual(exp_rv, rv)
 
 
 if __name__ == '__main__':
